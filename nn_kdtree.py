4b1698ed00719436238865335d64852fe0a318c2
Waveware <andrew.np@hotmail.com>
2024-05-03 22:37:20 +0930

    first attempt. test-sample returned wrong numbers but we'll see.

--- nn_kdtree.py
+++ nn_kdtree.py
@@ -1,54 +1,138 @@
 
+import sys
 import argparse
 import numpy as np
+import pandas as pd
+from math import dist
 
-a = """""
+s = sys.stdout
+
 class Node:
     def __init__(self):
         self.d = None
         self.val = None
-        self.left = None
-        self.right = None
-
-    def __init__(self, dimension, value, point):
-        self.d = dimension
-        self.val = value
-        self.p = point
+        self.p = None
         self.left = None
         self.right = None
 
 # Require a set of points P of M dimensions and current depth D
-def BuildKdTree(P, D):
+def BuildKdTree(P, D) -> Node:
+    M = 11
     # base case: Null and one data point
-    if (P.val == None): # P is empty
-        return None
+    #print("D: ", str(D))
+    if (P.size <= 0): # P is empty
+        #print('----empty')
+        return Node()
     elif(P.size == 1): # P has one data point
         # create a new node
         # node.d = d
         # node.val = val
         # node.point = current.point
-        return Node(D, P[0][D], P[0])
+        d = D%M
+        #print("---leaf:", str(P[0]))
+        N = Node()
+        N.d = d
+
+        df_P = pd.DataFrame(P)
+        df_P_values = df_P.to_numpy()
+
+        N.p = df_P_values[0]
+        N.val = df_P_values[0][d]
+        return N
+    
     else:
         # d <- D mod M
         d = D % M
+        #print("-d:", str(d))
+
         # val <- Median value along dimension d among points in P
-        point = np.median(P, axis=d)
-        val = point[d]
+        dfts = pd.DataFrame(P)
+        sorted = dfts.sort_values(dfts.columns[d], axis=0)
+        
+        if(P.size==2):
+            point = sorted.values[0]
+            val = sorted.values[0][d]
+        else:
+            point = sorted.values[len(sorted.values)//2]
+            val = point[d]
+        #print("point:", str(point), "val:", str(val))
+        
         # Create a new node
         # node.d <- d   node.val <- val
         # node.point <- point at the median along dimension d
         # node.left <- BuildKdTree(points in P for which value at dimension d is less than or equal to val, D+1)
         # node.right <- BuildKdTree(points in P for which value at dimension d is greater than val, D+1)
+        i = 0
         ltoeq_points = []
         gt_points = []
-        for points in P:
-            if (points[d] >= val):
-                ltoeq_points = 
+        for p in P:
+            if (p[d] <= val):
+                ltoeq_points.append(i)
+                i+=1
             else:
-                gt_points = P
-        Node(d, val, point, BuildKdTree(ltoeq_points, D+1), BuildKdTree(gt_points, D+1))
-        # return node
-"""""
+                gt_points.append(i)
+                i+=1
+        #print("--ltoeq:", str(ltoeq_points), "gt:", str(gt_points))
+        N = Node()
+        N.d = d
+        N.val = val
+        N.p = point
+        left = BuildKdTree(P[ltoeq_points], D+1)
+        right = BuildKdTree(P[gt_points], D+1)
+        N.left = left
+        N.right = right
+        #print(N)
+        return N
+
+# Require the root of the KdTree, point of query, and trace value (0 or 1)
+def QueryTree(tree, query_point, trace):
+    # trace will assist with debugging.
+    if(trace == 1):
+        print("Query point:", str(query_point))
+    nearest_node = QueryTreeSearch(tree, query_point, 10000, None, trace)
+    return nearest_node
+
+# Recursive function called by Query Tree
+def QueryTreeSearch(this_node, query_point, nearest_distance, nearest_node, trace):
+    if (trace == 1):
+        print("-Current Node Point:", str(this_node.p))
+        print("-Nearest Distance:", nearest_distance)
+    if (this_node != None): # does this node exist
+        # calculate distance and comparing with nearest
+        if (trace == 1):
+            print("--Calculating Distance of Q with C")
+            print("--  query_point            =", str(query_point))
+            print("--  this_node.p[0:11]      =", str(this_node.p[0:11]))
+        distance = dist(query_point, this_node.p[0:11])
+        if(trace == 1):
+            print("--  distance from this_node to query =", distance)
+        if (distance < nearest_distance):
+            if(trace == 1):
+                print("---updated nearest node and distance")
+            nearest_node = this_node
+            nearest_distance = distance
+
+        if (this_node.left == None and this_node.right == None):
+            # must be a leaf
+            if(trace == 1):
+                print("------ leaf node reached. Return.")
+                print("------ nearest distance =", nearest_distance)
+            return this_node
+        else: 
+            # Traverse to deeper nodes
+            d = this_node.d # dimension
+            check_query_value = query_point[d] <= this_node.val
+            if (check_query_value):
+                if(trace == 1):
+                    print("-----checking left subtree...")
+                return QueryTreeSearch(this_node.left, query_point, nearest_distance, nearest_node, trace)           
+            else:
+                if (trace == 1):
+                    print("-----checking right subtree...")
+                return QueryTreeSearch(this_node.right, query_point, nearest_distance, nearest_node, trace)            
+
+
+
 
 def main():
 
@@ -64,8 +148,31 @@ def main():
     parser.add_argument('dimension', type=int)
     args = parser.parse_args()
 
-    test_set = np.genfromtxt(args.test, names=True)
     train_set = np.genfromtxt(args.train, names=True)
+    test_set = np.genfromtxt(args.test, names=True)
+
+    tsdf = pd.DataFrame(test_set)
+    ts = tsdf.to_numpy()
+
+    dim = 0
+
+    tree = BuildKdTree(train_set, dim)
+
+    tr = 1 # trace value
+
+    #print(QueryTree1NN(tree, test_set[0], tr))
+    #print(ts.shape[0])
+    #closest = QueryTree(tree,ts[0], tr)
+    #print(closest.p[-1])
+
+    wine_quality_predictions = []
+    for wine in range(ts.shape[0]):
+        closest_wine_approximation = QueryTree(tree, ts[wine], tr)
+        wine_quality_predictions.append(closest_wine_approximation.p[-1])
+    
+    print("Wine predictions:")
+    print(wine_quality_predictions)
+
 
 if __name__ == "__main__":
     main()
