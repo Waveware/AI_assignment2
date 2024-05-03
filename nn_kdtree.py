f6c45ff52793bfae3fa12709b85f7331c2376b36
Waveware <andrew.np@hotmail.com>
2024-05-04 00:38:17 +0930

    improvements to the median calculations led to some better  (maybe)

--- nn_kdtree.py
+++ nn_kdtree.py
@@ -4,6 +4,11 @@ import argparse
 import numpy as np
 import pandas as pd
 from math import dist
+from math import ceil
+from statistics import median
+
+# trace value for debugging
+tr = 0
 
 s = sys.stdout
 
@@ -18,18 +23,21 @@ class Node:
 # Require a set of points P of M dimensions and current depth D
 def BuildKdTree(P, D) -> Node:
     M = 11
+    if(tr == 1):
+        print("D:", str(D))
     # base case: Null and one data point
-    #print("D: ", str(D))
     if (P.size <= 0): # P is empty
-        #print('----empty')
+        if (tr == 1):
+            print("P is empty")
         return Node()
+    
     elif(P.size == 1): # P has one data point
         # create a new node
         # node.d = d
         # node.val = val
         # node.point = current.point
         d = D%M
-        #print("---leaf:", str(P[0]))
+
         N = Node()
         N.d = d
 
@@ -38,24 +46,29 @@ def BuildKdTree(P, D) -> Node:
 
         N.p = df_P_values[0]
         N.val = df_P_values[0][d]
+        if (tr == 1):
+            print('----leaf node reached')
         return N
     
     else:
-        # d <- D mod M
+        # dimension
         d = D % M
-        #print("-d:", str(d))
 
-        # val <- Median value along dimension d among points in P
-        dfts = pd.DataFrame(P)
-        sorted = dfts.sort_values(dfts.columns[d], axis=0)
-        
-        if(P.size==2):
-            point = sorted.values[0]
-            val = sorted.values[0][d]
+        # val is the median value
+        # point is the point at the median value at dimension d
+        df = pd.DataFrame(P)
+        df.sort_values(by=df.columns.values[d])
+        df.reset_index()
+        median_index = median(df.index.values)
+        if (P.shape[0] == 2):
+            point = df.values[0]
+            val = (point[d] + df.values[1][d])/2
         else:
-            point = sorted.values[len(sorted.values)//2]
+            point = df.values[int(ceil(median_index))]
             val = point[d]
-        #print("point:", str(point), "val:", str(val))
+        
+        if(tr == 1):
+            print("point:", str(point), "val:", str(val), "dim:", str(d))
         
         # Create a new node
         # node.d <- d   node.val <- val
@@ -63,16 +76,21 @@ def BuildKdTree(P, D) -> Node:
         # node.left <- BuildKdTree(points in P for which value at dimension d is less than or equal to val, D+1)
         # node.right <- BuildKdTree(points in P for which value at dimension d is greater than val, D+1)
         i = 0
+        df_P_sorted = df.to_numpy()
         ltoeq_points = []
         gt_points = []
-        for p in P:
-            if (p[d] <= val):
+        for p in range(len(df_P_sorted)):
+            if (tr == 1):
+                print(df_P_sorted[p][d])
+            if (df_P_sorted[p][d] <= val):
                 ltoeq_points.append(i)
                 i+=1
             else:
                 gt_points.append(i)
                 i+=1
-        #print("--ltoeq:", str(ltoeq_points), "gt:", str(gt_points))
+        if(tr == 1):        
+            print("--ltoeq:", str(ltoeq_points))
+            print("--gt:", str(gt_points))
         N = Node()
         N.d = d
         N.val = val
@@ -159,9 +177,6 @@ def main():
 
     # construct tree
     tree = BuildKdTree(train_set, dim)
-
-    # trace value for debugging
-    tr = 0
 
     # input test on trained KdTree and find 1NN for each sample.
     wine_quality_predictions = []
