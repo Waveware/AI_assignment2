7d82e58ef1ca85aa2ce33b24d7f40c0af749eacc
Waveware <andrew.np@hotmail.com>
2024-05-05 23:00:28 +0930

    Updated the median function again..

--- nn_kdtree.py
+++ nn_kdtree.py
@@ -19,8 +19,24 @@ class Node:
         self.p = np.array([False, False, False, False])
         self.left = None
         self.right = None
+    def is_empty(self):
+        if (self.d == None):
+            return False
+        if (self.val == None):
+            return False
+        if (self.p.any() == False):
+            return False
+        if (self.left == None):
+            return False
+        if (self.right == None):
+            return False
+        return True
+    def is_leaf(self):
+        if (self.left == None and self.right == None):
+            return True
+        return False
 
-# Require a set of points P of M dimensions and current depth D
+# KdTree algorithm from the assignment description
 def BuildKdTree(P, D) -> Node:
     M = 11
     if(tr == 1):
@@ -29,23 +45,24 @@ def BuildKdTree(P, D) -> Node:
     if (P.size <= 0): # P is empty
         if (tr == 1):
             print("P is empty")
-        return Node()
+        return None
     
     elif(P.size == 1): # P has one data point
-        # create a new node
-        # node.d = d
-        # node.val = val
-        # node.point = current.point
         d = D%M
 
+        # create a new node
         N = Node()
+        # node.d = d
         N.d = d
 
         df_P = pd.DataFrame(P)
         df_P_values = df_P.to_numpy()
 
-        N.p = df_P_values[0]
+        # node.val = val
         N.val = df_P_values[0][d]
+        # node.point = current.point
+        N.p = df_P_values[0]
+
         if (tr == 1):
             print('----leaf node reached')
         return N
@@ -65,16 +82,11 @@ def BuildKdTree(P, D) -> Node:
             val = (point[d] + df.values[1][d])/2
         else:
             point = df.values[int(ceil(median_index))]
-            val = point[d]
+            val = median(df[df.columns[d]])
         
         if(tr == 1):
             print("point:", str(point), "val:", str(val), "dim:", str(d))
         
-        # Create a new node
-        # node.d <- d   node.val <- val
-        # node.point <- point at the median along dimension d
-        # node.left <- BuildKdTree(points in P for which value at dimension d is less than or equal to val, D+1)
-        # node.right <- BuildKdTree(points in P for which value at dimension d is greater than val, D+1)
         i = 0
         df_P_sorted = df.to_numpy()
         ltoeq_points = []
@@ -91,18 +103,26 @@ def BuildKdTree(P, D) -> Node:
         if(tr == 1):        
             print("--ltoeq:", str(ltoeq_points))
             print("--gt:", str(gt_points))
+
+        # Create a new node
         N = Node()
+        # node.d <- d | node.val <- val | node.point <- point at the median along dimension d
         N.d = d
         N.val = val
         N.p = point
+
+        # node.left <- BuildKdTree(points in P for which value at dimension d is less than or equal to val, D+1)
         left = BuildKdTree(P[ltoeq_points], D+1)
+
+        # node.right <- BuildKdTree(points in P for which value at dimension d is greater than val, D+1)        
         right = BuildKdTree(P[gt_points], D+1)
+        
         N.left = left
         N.right = right
-        #print(N)
+
         return N
 
-# Require the root of the KdTree, point of query, and trace value (0 or 1)
+# unused
 def QueryTree(tree, query_point, trace):
     # trace will assist with debugging.
     if(trace == 1):
@@ -110,7 +130,7 @@ def QueryTree(tree, query_point, trace):
     nearest_node = QueryTreeSearch(tree, query_point, 10000, None, trace)
     return nearest_node
 
-# Recursive function called by Query Tree
+# unused
 def QueryTreeSearch(this_node, query_point, nearest_distance, nearest_node, trace):
     if (trace == 1):
         print("-Current Node Point:", str(this_node.p))
@@ -151,6 +171,77 @@ def QueryTreeSearch(this_node, query_point, nearest_distance, nearest_node, trac
     else:
         return nearest_node
 
+#   Query NN Search from the lecture slide
+def QueryNNSearch(tree, query):
+    # set the current node to the tree (root)
+    node = tree
+    node_stack = [tree]
+    if (tr == 2):
+        print("Starting QNNSearch for", str(query))
+        print("With node:", str(tree))
+    # keep going down until a leaf node is reached
+    while (node.is_leaf() == False):
+        if (tr == 2):
+            print('Going down:',  str(node), "| d:", str(node.d), "| p:", str(node.p))
+        if (query[node.d] < node.val):
+            if (node.left == None):
+                continue
+            node = node.left
+            node_stack.append(node)
+        else:
+            if (node.right == None):
+                continue
+            node = node.right
+            node_stack.append(node)         
+    
+    # leaf node is now current best node
+    if (tr == 2):
+        print("best_node:", str(node), "| d:", str(node.d), "| p:", str(node.p))
+    while False in node.p:
+        node = node_stack.pop()
+        if (tr == 2):
+            print('-- False found. choosing previous node:', str(node), "| d:", str(node.d), "| p:", str(node.p))
+    best_node = node
+    best_distance = dist(query, best_node.p[0:11])
+    if (tr == 2):
+        print("leaf node reached! P:", str(node.p))
+        print("leaf node best_distance:", str(best_distance))
+    
+    while (node != tree):
+        if (len(node_stack) == 0):
+            return best_node
+        node = node_stack.pop() # current node
+        if (node.is_empty()):
+            continue
+
+        lowerbound_distance = query[node.d] - node.val
+        if (lowerbound_distance < 0):
+            check_distance = dist(query, node.p[0:11])
+            if (best_distance > check_distance):
+                best_distance = check_distance
+                best_node = node
+            if (query[node.d] < node.val):
+                if(node.left == None):
+                    continue
+                else:
+                    if (tr == 2):
+                        print("going left")
+                    node = node.left
+                    node_stack.append(node)
+                if (node.right == None):
+                    continue
+                else:
+                    if(tr == 2):
+                        print("going right")
+                    node = node.right
+                    node_stack.append(node)         
+
+    
+    if(tr == 2):
+        print("root node is reached!")
+
+    return best_node
+
 
 
 def main():
@@ -182,6 +273,7 @@ def main():
     wine_quality_predictions = []
     for wine in range(ts.shape[0]):
         closest_wine_approximation = QueryTree(tree, ts[wine], tr)
+        #closest_wine_approximation = QueryNNSearch(tree, ts[wine])
         wine_quality_predictions.append(closest_wine_approximation.p[-1])
 
     #print("Wine predictions:")
